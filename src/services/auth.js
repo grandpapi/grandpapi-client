95f373c004f98048f5cac11ada5f8d62c3b4785e
Ben Parisot <me@benparisot.com>
2019-06-13 13:49:41 -0700

    auth service passes test

---
+++ src/services/auth.js
@@ -0,0 +1,31 @@
+import { WebAuth } from 'auth0-js';
+
+const auth0 = new WebAuth({
+  domain: process.env.AUTH_DOMAIN,
+  clientID: process.env.AUTH_CLIENT_ID,
+  redirectUri: process.env.AUTH_REDIRECT_URI,
+  responseType: 'token id_token',
+  scope: 'openid profile'
+});
+
+export const login = () => auth0.authorize();
+
+export const handleAuth = () => {
+  return new Promise((resolve, reject) => {
+    auth0.parseHash((error, results) => {
+      if(results && results.accessToken && results.idToken) {
+        auth0.client.userInfo(results.accessToken, (err, profile) => {
+          if(err) return reject('Could not get user profile');
+          resolve({
+            username: profile.name,
+            token: results.accessToken,
+            image: profile.picture
+          });
+        });
+      }
+      else {
+        reject('Could not log in');
+      }
+    });
+  });
+};
