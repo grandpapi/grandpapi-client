4d1a8f3d5856073a6ad1772bd4c9433713b3a42b
Ben Parisot <me@benparisot.com>
2019-06-13 14:00:17 -0700

    write withSession container and session reducer

---
+++ src/containers/auth/withSession.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { login } from '../../services/auth';
+import { getToken } from '../../selectors/sessionSelectors';
+import { connect } from 'react-redux';
+
+export const withSession = Component => {
+  class WithSession extends PureComponent {
+        static propTypes = {
+          token: PropTypes.string.isRequired
+        }
+
+        componentDidMount() {
+          if(!this.props.token) login();
+        }
+
+        render() {
+          if(!this.props.token) return <h1>Sorry you need to log in mate</h1>;
+          return <Component />;
+        }
+  }
+
+  const mapStateToProps = state => ({
+    token: getToken(state)
+  });
+
+  return connect(
+    mapStateToProps
+  )(WithSession);
+};
