35bb8cbe1ad6456602b6c986891d3c83ff305e12
Ben Parisot <me@benparisot.com>
2019-06-13 15:12:47 -0700

    User can log out

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { login } from '../services/auth';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <button onClick={login}>Log In / Sign Up</button>
+    </>
   );
 }
