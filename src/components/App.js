f3dbe52f1d672bc27d454c0c10cc03deea9c6f1a
Ben Parisot <me@benparisot.com>
2019-06-13 12:05:20 -0700

    set up home and app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Callback from '../containers/auth/Callback';
+// import { withSession } from '../containers/auth/withSession';
+import Home from '../components/Home';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/callback" component={Callback} />
+      </Switch>
+    </Router>
+  );
 }
   
---
+++ src/containers/auth/Callback.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 
+
+export default class Callback extends PureComponent {
+
+  render() {
+    return (
+      <h1>Callback</h1>
+    );
+  }
+}
