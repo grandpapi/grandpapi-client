eae78756af876f586f5d77f79bd806429d11d763
Ben Parisot <me@benparisot.com>
2019-06-14 11:54:33 -0700

    imported instructions to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Splash from '../components/home/Splash';
 import Footer from '../components/all/Footer';
+import InstructDb from '../components/home/instructions/InstructDb';
+import InstructModel from '../components/home/instructions/InstructModel';
+import InstructData from '../components/home/instructions/InstructData';
+import InstructDeploy from '../components/home/instructions/InstructDeploy';
+import InstructStartBuilding from '../components/home/instructions/InstructStartBuilding';
 
 export default function Home() {
   return (
     <>
       <Splash />
+      <section className="instructions">
+        <InstructDb />
+        <InstructModel />
+        <InstructData />
+        <InstructDeploy />
+        <InstructStartBuilding />
+      </section>
       <Footer />
     </>
   );
