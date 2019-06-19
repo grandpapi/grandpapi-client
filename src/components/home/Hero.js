6cb027dd40a8617586faffc7300441e82b2a9393
Ben Parisot <me@benparisot.com>
2019-06-18 17:02:59 -0700

    WIP style splash header nav

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { H1, HeroSection } from '../../styles';
 
 export default function Hero() {
   return (
-    <section>
-      <h1>Make an app with MegaNap.</h1>
+    <HeroSection>
+      <H1>Make an app with Mega Nap.</H1>
       <p>Easily enter your data, then receive endpoints.</p>
       <Link to="/login">Start Building</Link>
-    </section>
+    </HeroSection>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+//HOME PAGE COMPONENTS
+
+export const HeroSection = styled.section`
+  background-color: #b0e2ab;
+  padding-top: 2rem;
+`;
+
+export const H1 = styled.h1`
+  font-family: 'Oxygen', sans-serif;
+  font-size: 2.875rem;
+  color: #442c1d;
+  font-weight: bold;
+`;
+
+export const SplashHeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #b0e2ab;
+  padding: .8rem 1rem;
+  padding-right: 1.5rem;
+  font-family: 'Montserrat', sans-serif;
+  text-transform: uppercase;
+
+`;
+
+export const StyledNavLink = styled.span`
+  color: red;
+  text-decoration: none;
+`;
+
+export const NavLogo = styled.img`
+  src: 'assets/logo-pink.png';
+  height: 4.4375rem;
+  width: auto;
+`;
+//
