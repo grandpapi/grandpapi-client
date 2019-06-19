9502247bf44ba9fd29c7ffad2676f275e94bca78
Tommy Tran <tranttommy95@gmail.com>
2019-06-19 16:19:01 -0700

    refactored to remove model reducers and extraneous model things

--- src/selectors/sessionSelectors.js
+++ src/selectors/sessionSelectors.js
@@ -3,3 +3,4 @@ export const selectNickname = state => state.session.nickname;
 export const selectUserId = state => state.session.userId;
 export const selectCurrentDatabase = state => state.session.currentDatabase;
 export const selectCurrentModel = state => state.session.currentModel;
+export const selectCurrentModelSchema = state => selectCurrentModel(state).mdlSchema;
