e2c2d51fa3a99b5891f1173c40a720d75dcbb5af
Tommy Tran <tranttommy95@gmail.com>
2019-06-19 10:21:10 -0700

    refactor Links for create routes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,9 @@ import Callback from '../containers/auth/Callback';
 import Home from '../pages/Home';
 import { withSession } from '../containers/auth/withSession';
 import Dashboard from '../pages/Dashboard';
-import Create from '../pages/Create';
+import DatabaseCreation from '../pages/DatabaseCreation';
+import ModelCreation from '../pages/ModelCreation';
+// import DataCreation from '../pages/DataCreation';
 import Docs from '../pages/Docs';
 import AboutApp from '../pages/AboutApp';
 import PublicApis from '../pages/PublicApis';
@@ -27,7 +29,9 @@ export default function App() {
         <Route exact path="/apis" component={PublicApis} />
         <Route path="/dashboard/:dbName" component={withSession(DatabaseDetail)}/>
         <Route exact path="/dashboard" component={withSession(Dashboard)} />
-        <Route exact path="/create" component={withSession(Create)} />
+        <Route exact path="/create/database" component={withSession(DatabaseCreation)} />
+        <Route exact path="/create/model" component={withSession(ModelCreation)} />
+        {/* <Route exact path="/create/data" component={withSession(DataCreation)} /> */}
       </Switch>
     </Router>
   );
