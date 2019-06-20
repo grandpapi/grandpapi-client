06f6a4ccbad39fe71fbf06fd8ee0661feef5888d
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-19 22:49:31 -0700

    can style GlobalHeader

--- src/components/all/GlobalHeader.js
+++ src/components/all/GlobalHeader.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Nav from './Nav';
 import { Link } from 'react-router-dom';
+import pinkLogo from '../../../assets/logo-pink.png';
+import { HeaderContainer, NavLogo, NavContainer } from '../../styles';
 
 export default function GlobalHeader() {
   return (
-    <header>
-      <img src='test.png' />
-      <Nav/>
+    <HeaderContainer>
+      <Link to="/"><NavLogo src={pinkLogo} /></Link>
+      <NavContainer>
+        <Nav/>
+      </NavContainer>
       <Link to="/login">Log In | Sign Up</Link>
-    </header>
+    </HeaderContainer>
   );
 }
