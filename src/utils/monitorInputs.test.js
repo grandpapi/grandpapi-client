9febada25f3cca7f7ce0a51f5c2f518330aa28d8
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-20 16:37:14 -0700

    making tests pretty

--- src/utils/monitorInputs.test.js
+++ src/utils/monitorInputs.test.js
@@ -4,30 +4,30 @@ describe('Testing Monitor Inputs function', () => {
   it('will accept a good name', () => {
     const check = monitorInputs('some-thing');
 
-    expect(check).toBeTruthy();
-    
+    expect(check).toBeTruthy();  
   });
+
   it('will reject a name with spaces', () => {
     const check = monitorInputs('something awesome');
 
-    expect(check).toBeFalsy();
-    
+    expect(check).toBeFalsy();  
   });
+
   it('will reject a name with more than 14 characters', () => {
     const check = monitorInputs('abcdefghijklmnopqrs');
 
-    expect(check).toBeFalsy();
-    
+    expect(check).toBeFalsy();  
   });
+
   it('will accept a mdlSchema with good names', () => {
     const check = monitorInputs('clean', {
       'alpha': 'String',
       'beta': 'Boolean'
     });
 
-    expect(check).toBeTruthy();
-    
+    expect(check).toBeTruthy(); 
   });
+
   it('will reject a mdlSchema with spaces in the names', () => {
     const check = monitorInputs('clean', {
       'alp ha': 'String',
@@ -35,7 +35,6 @@ describe('Testing Monitor Inputs function', () => {
     });
 
     expect(check).toBeFalsy();
-    
   });
 
   it('will reject a mdlSchema with more than 14 characters in the names', () => {
@@ -44,10 +43,6 @@ describe('Testing Monitor Inputs function', () => {
       'beta': 'Boolean'
     });
 
-    expect(check).toBeFalsy();
-    
+    expect(check).toBeFalsy(); 
   });
-
-
-
 });
