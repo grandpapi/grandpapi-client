7f7a767569fe77277663162b16d8f7a3761317f4
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-20 16:35:21 -0700

    monitorInput testing

---
+++ src/utils/monitorInputs.js
@@ -0,0 +1,28 @@
+export default function monitorInputs(name, mdlSchema = {}) {
+  if(name.includes(' ')) {
+    // window.alert('You cannot include spaces; try using camelCase or kabob-case');
+    return false;
+  }
+  if(name.length > 14) {
+    // window.alert('One of your fields exceeds the maximum character length of 14');
+    return false;
+  }
+
+  if(mdlSchema) {
+    const monitorArray = Object.keys(mdlSchema).map(key => {
+      if(key.includes(' ') || key.length > 14) {
+        return false;
+      }
+      return true;
+    });
+
+    if(monitorArray.includes(false)) {
+      // window.alert('One of your fields includes a space or exceeds the maximum character length of 14');
+      return false;
+    }
+
+    return true;
+  }
+
+}
+
