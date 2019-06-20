350e1015236ffc217c7ed832cb44090956144831
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-20 13:58:15 -0700

    double quotes are people too

--- src/components/all/Footer.js
+++ src/components/all/Footer.js
@@ -1,4 +1,3 @@
-/* eslint-disable quotes */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FooterContainer, StyledNavUl, FooterTagline } from '../../styles';
@@ -7,10 +6,10 @@ export default function Footer() {
   return (
     <FooterContainer>
       <StyledNavUl>
-        <Link to={"/table3"}><li>About Us</li></Link>
-        <Link to={"/careers"}><li>Careers</li></Link>
-        <Link to={"/privacy"}><li>Privacy Policy</li></Link>
-        <Link to={"/apis"}><li>Public APIs</li></Link>
+        <Link to="/table3"><li>About Us</li></Link>
+        <Link to="/careers"><li>Careers</li></Link>
+        <Link to="/privacy"><li>Privacy Policy</li></Link>
+        <Link to="/apis"><li>Public APIs</li></Link>
       </StyledNavUl>
       <FooterTagline>
         <p>Mega Nap: For the love of apps</p>
