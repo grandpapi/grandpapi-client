7181706725d825e5435665eaff00ded09e1e26ba
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-19 22:49:46 -0700

    About pages have GlobalHeader

--- src/pages/AboutApp.js
+++ src/pages/AboutApp.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Nav from '../components/all/Nav';
+import Footer from '../components/all/Footer';
+import GlobalHeader from '../components/all/GlobalHeader';
 
 export default function AboutApp() {
   return (
     <>
-      <Nav />
+      <GlobalHeader />
       <h1>About App</h1>
+      <Footer />
     </>
   );
 }
--- src/pages/AboutTable3.js
+++ src/pages/AboutTable3.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from '../components/all/Nav';
 import Footer from '../components/all/Footer';
+import GlobalHeader from '../components/all/GlobalHeader';
 
 export default function AboutTable3() {
   return (
     <>
+      <GlobalHeader />
       <Nav />
       <h1>Table 3</h1>
       <p>Grandpapi took a MegaNap</p>
