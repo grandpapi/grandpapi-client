1304617a40fc7aba891aac02cfab95897d88f7fa
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-20 19:34:17 -0700

    global header updates session state for breadcrumbs

--- src/components/all/GlobalHeader.js
+++ src/components/all/GlobalHeader.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import logoPink from '../../../assets/logo-pink.png';
 import { NavLogo, GlobalHeaderContainer, HeroButton, BodyButton } from '../../styles';
 import styles from '../../styles.css';
 import { logout } from '../../services/auth';
+import { navigateDashboard } from '../../actions/sessionActions';
 
-export default function GlobalHeader() {
+function GlobalHeader({ dashboardClick }) {
   return (
     <GlobalHeaderContainer>
       <Link to="/"><NavLogo src={logoPink} /></Link>
       <section>
-        <BodyButton header><Link to="/dashboard" className={styles.linkInButton}>My Dashboard</Link></BodyButton>
+        <BodyButton header><Link to="/dashboard" className={styles.linkInButton} onClick={() => dashboardClick()}>My Dashboard</Link></BodyButton>
         <HeroButton onClick={logout}>Log Out</HeroButton>
       </section>
     </GlobalHeaderContainer>
   );
 }
+
+GlobalHeader.propTypes = {
+  dashboardClick: PropTypes.string.isRequired
+};
+
+const mapDispatchToProps = dispatch => ({
+  dashboardClick() {
+    dispatch(navigateDashboard());
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(GlobalHeader);
